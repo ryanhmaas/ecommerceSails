a09db83ea720f7002fdb8b5fcbcf3b3c4e236359
Ryan Maas <ryan.h.maas@gmail.com>
2015-11-25 13:25:23 -0500

    schema ideas

---
+++ api/models/product.js
@@ -0,0 +1,14 @@
+//product.js
+
+//TODO: Schema
+
+/*
+p_key: primary key
+sku/product number: int/string
+price: int/double
+name: string
+description: string
+
+parent-child relationships?
+
+*/
---
+++ api/models/user.js
@@ -0,0 +1,15 @@
+//http://www.geektantra.com/2013/08/implement-passport-js-authentication-with-sails-js/
+
+//user model
+
+//Schema:
+
+/*
+c_key: primary key
+username: String
+password: String
+first_name: String
+last_Name: String
+email: String
+login_count: int
+*/
